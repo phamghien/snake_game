91ec3096d29f080c13438a3c25f3af94b4d45888
Grace Pham <phamghien@gmail.com>
2025-06-04 08:40:11 -0400

    Collision detection and scoreboard

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import time
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 # Create screen
 screen = Screen()
@@ -9,8 +11,10 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-# Create snake
+# Create snake, food, scoreboard
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 # Event Listeners
 screen.listen()
@@ -25,8 +29,23 @@ game_on = True
 while game_on:
     screen.update()  # Update screen after each segment is moved
     time.sleep(0.1)
-
     snake.move()
 
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
