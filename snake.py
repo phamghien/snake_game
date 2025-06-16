291be7ce0ac13f6a400fcda6404ac4028a877f7f
Grace Pham <phamghien@gmail.com>
2025-06-16 14:55:10 -0400

    Saving high score
    Add data.txt to hold the high score.
    Read from the file to retrieve high score.
    Write to file to save high score.

--- snake.py
+++ snake.py
@@ -25,6 +25,13 @@ class Snake:
         new_segment.goto(position)
         self.segments.append(new_segment)
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear() # Clear all segments
+        self.create_snake() # Create new snake at starting position
+        self.head = self.segments[0]
+
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
